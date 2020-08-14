a8cf752ef683370b14318c856c46a132b1b9037f
mgkang134 <mgkang134@gmail.com>
2020-08-14 13:22:27 +0900

    JPA 엔티티 생성

---
+++ src/main/java/com/example/demo/domain/item/Item.java
@@ -0,0 +1,38 @@
+package com.example.demo.domain.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToOne;
+
+import com.example.demo.domain.Category;
+
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="dtype")
+public abstract class Item {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="item_id")
+	private Long id;
+	
+	private String name;
+	private int price;
+	private int stockQuantity;
+	
+	@ManyToMany(mappedBy = "items")
+	private List<Category> categories = new ArrayList<>();
+	
+	
+}
