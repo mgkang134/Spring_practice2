5dae2f48569252c42f53c5671961ea42a49a0ea3
mgkang134 <mgkang134@gmail.com>
2020-08-15 21:58:38 +0900

    상품 엔티티 작성

--- src/main/java/com/example/demo/domain/item/Item.java
+++ src/main/java/com/example/demo/domain/item/Item.java
@@ -16,6 +16,7 @@ import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 import com.example.demo.domain.Category;
+import com.example.demo.exception.NotEnoughStockException;
 
 @Entity
 @Inheritance(strategy=InheritanceType.SINGLE_TABLE)
@@ -33,6 +34,25 @@ public abstract class Item {
 	
 	@ManyToMany(mappedBy = "items")
 	private List<Category> categories = new ArrayList<>();
+	
+	
+	//==비즈니스 로직==//
+	
+	//재고 수량 증가
+	public void addStock(int quantity) {
+		this.stockQuantity += quantity;
+	}
+	
+	//재고 수량 감소
+	public void removeStock(int quantity) {
+		int restStock = this.stockQuantity - quantity;
+		if(restStock < 0) {
+			throw new NotEnoughStockException("need more stock");
+		}else {
+			this.stockQuantity = restStock;
+		}
+		
+	}
 	
 	
 }
