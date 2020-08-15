34176ced855bc719b2619cb89957ba07566448ef
mgkang134 <mgkang134@gmail.com>
2020-08-15 10:38:29 +0900

    Member Service 작성

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+
+	@Autowired
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+	
+	//회원가입
+	@Transactional(readOnly = true)
+	public Long join(Member member) {
+		validateDuplicateMember(member);
+		memberRepository.save(member);
+		return member.getId();
+	}
+
+	private void validateDuplicateMember(Member member) {
+		List<Member> result = memberRepository.findByName(member.getName());
+		if(!result.isEmpty()) {
+			throw new IllegalStateException("이미 존재하는 회원입니다.");
+		}
+	}
+	
+	//회원 전체 조회
+	private List<Member> findMembers(){
+		return memberRepository.findAll();
+	}
+	
+	//회원 한 건 조회
+	private Member findOne(Long memberId) {
+		return memberRepository.findOne(memberId);
+	}
+	
+}
