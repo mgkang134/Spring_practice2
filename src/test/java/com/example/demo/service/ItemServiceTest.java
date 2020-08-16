0c9c5ac84de6c86b7392e5aa0fa6bef038fb26e3
mgkang134 <mgkang134@gmail.com>
2020-08-16 09:30:14 +0900

    Item Service 테스트코드 작성

---
+++ src/test/java/com/example/demo/service/ItemServiceTest.java
@@ -0,0 +1,48 @@
+package com.example.demo.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.item.Album;
+import com.example.demo.repository.ItemRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+class ItemServiceTest {
+
+	@Autowired private ItemService itemService;
+	@Autowired private ItemRepository itemRepository;
+	
+	@Test
+	void 아이템추가() {
+		//given
+		Album item = new Album();
+		item.setArtist("윤도현");
+		
+		//when
+		itemService.saveItem(item);
+		
+		//then
+		assertThat(itemRepository.findAll().size()).isEqualTo(1);
+	}
+
+	@Test
+	void testSaveItem() {
+	}
+
+	@Test
+	void testFindItem() {
+	}
+
+	@Test
+	void testFindOne() {
+	}
+
+}
