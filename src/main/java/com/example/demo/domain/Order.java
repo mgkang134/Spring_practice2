6f455365215da2c9bba3d1ae2333fdaf405deb2b
mgkang134 <mgkang134@gmail.com>
2020-08-16 10:59:54 +0900

    Order, OrderItem 엔티티 작성

--- src/main/java/com/example/demo/domain/Order.java
+++ src/main/java/com/example/demo/domain/Order.java
@@ -2,6 +2,7 @@ package com.example.demo.domain;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.persistence.CascadeType;
@@ -43,6 +44,49 @@ public class Order {
 	@Enumerated(EnumType.STRING)
 	private OrderStatus status;	//주문 상태 [ORDER, CANCEL]
 	
+	
+	private void setStatus(OrderStatus order) {
+		this.status = order;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public List<OrderItem> getOrderItems() {
+		return orderItems;
+	}
+
+	public void setOrderItems(List<OrderItem> orderItems) {
+		this.orderItems = orderItems;
+	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public Member getMember() {
+		return member;
+	}
+
+	public Delivery getDelivery() {
+		return delivery;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+
+
 	//연관관계 메서드
 	public void setMember(Member member) {
 		this.member = member;
@@ -59,6 +103,43 @@ public class Order {
 		delivery.setOrder(this);
 	}
 	
+	
+	
+	//==생성 메서드==//
+	public static Order createOrder(Member member, Delivery delivery, OrderItem...orderItems) {
+		Order order = new Order();
+		order.setMember(member);
+		order.setDelivery(delivery);
+		for(OrderItem orderItem : orderItems) {
+			order.addOrderItem(orderItem);
+		}
+		order.setStatus(OrderStatus.ORDER);
+		order.setOrderDate(LocalDateTime.now());
+		return order;
+	}
+
+	//==비즈니스 로직==//
+	
+	//주문 취소
+	public void cancel() {
+		if(delivery.getStatus() == DeliveryStatus.COMP) {
+			throw new IllegalStateException("이미 배송되어 취소가 불가능합니다.");
+		}
+		
+		this.setStatus(OrderStatus.ORDER);
+		for (OrderItem orderItem : orderItems) {
+			orderItem.cancel();
+		}
+	}
+	
+	//==조회 로직==//
+	public int getTotalPrice() {
+		return orderItems.stream().mapToInt(OrderItem::getTotalPrice).sum();
+	}
+
+	
+	
+
 	
 	
 	
